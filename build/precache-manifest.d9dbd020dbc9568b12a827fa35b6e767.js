809b7923287eace720cc729c2506402b114612e4
Meet Patel <meetaiims@yahoo.com>
2021-02-01 07:29:15 -0600

    add vercel.json

---
+++ build/precache-manifest.d9dbd020dbc9568b12a827fa35b6e767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e77b2fbace9b069e7d4e31b5c3a38d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c5c29648b80c3b23c25",
+    "url": "/static/css/main.258afe6b.chunk.css"
+  },
+  {
+    "revision": "61c16c1cbe636a95d836",
+    "url": "/static/js/2.e6c0cd3a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e6c0cd3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5c29648b80c3b23c25",
+    "url": "/static/js/main.e22767f8.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  API_BASE_URL: 'https://mighty-sea-18998.herokuapp.com',
+  /*
   API_BASE_URL: 'http://localhost:8000',
+  */
 };
